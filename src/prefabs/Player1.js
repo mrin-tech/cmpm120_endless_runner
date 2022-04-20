0c76ed5deed828e8fae16550c57980d6817f63e4
“Mrinmoyee <mrmishra@ucsc.edu>
2022-04-20 14:49:19 -0700

    added left+right movement

--- src/prefabs/Player1.js
+++ src/prefabs/Player1.js
@@ -3,6 +3,7 @@ class Player1 extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
 
         scene.add.existing(this);
+        this.moveSpeed = 3;
 
     }
 
@@ -10,6 +11,12 @@ class Player1 extends Phaser.GameObjects.Sprite {
         // add player core mechanics
 
         // running
+        // left/right movement
+        if(keyA.isDown && this.x >= borderUISize + this.width) {
+            this.x -= this.moveSpeed;
+        } else if (keyD.isDown && this.x <= game.config.width - borderUISize - this.width) {
+            this.x += this.moveSpeed;
+        }
 
         // sliding
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,11 @@ class Play extends Phaser.Scene {
 
     create() {
         this.runner = this.add.rectangle(game.config.width / 2, game.config.height* 3 / 4, 80, 160, 0xFFFFFF);
+
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update() {
