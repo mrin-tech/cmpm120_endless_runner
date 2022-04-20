7887221129abf4fc749303cbc795969e9ed41560
“Mrinmoyee <mrmishra@ucsc.edu>
2022-04-20 13:44:46 -0700

    basic player sprite

---
+++ src/prefabs/Player1.js
@@ -0,0 +1,20 @@
+class Player1 extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);
+
+    }
+
+    update() {
+        // add player core mechanics
+
+        // running
+
+        // sliding
+
+        // jumping
+
+        // double jump
+    }
+}
