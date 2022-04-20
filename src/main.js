d31e86a09a2aab2985004e79629251319281ff0d
matty217 <mathill@ucsc.edu>
2022-04-20 11:05:24 -0700

    initializing main files

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 960, //640,
+    height: 630, //420
+    scene: [ Menu, Play, Play2 ]
+}
+
+
+let game = new Phaser.Game(config);
+
+// reserving keyboard variables
+let keyR, keyRIGHT, keyLEFT, keyP, keyA, keyD, keyF, keyUP;
+
+let borderUISize = 40;
+let borderPadding = borderUISize / 3;
