e591f9f3a112ca160453a0d38d28d087b3b6934a
matty217 <mathill@ucsc.edu>
2022-04-22 01:47:53 -0700

    implementing animations

--- src/prefabs/Player1.js
+++ src/prefabs/Player1.js
@@ -7,27 +7,66 @@ class Player1 extends Phaser.Physics.Arcade.Sprite {
         this.moveSpeed = 3;
         this.jumps = 0;
         this.Jumping = false;
+        this.Running = false;
+        this.idleLooping = false;
+        this.runLooping = false;
+    }
+
+    preload() {
+        // this.load.spritesheet('runner', 'assets/Player-Sprites/idle-run-temp.png', {
+        //     frameWidth: 32,
+        //     frameHeight: 32
+        // });
     }
 
     create() {
         player.setBounce(0.5);
         player.setCollideWorldBounds(true);
+        
     }
 
     update() {
         // add player core mechanics
+        this.onGround = this.body.touching.down;        //checks if player is standing on solid ground
+        console.log(this.onGround);
+        console.log(this.Running);
+        //standing
+        if (this.onGround && this.Running == false && this.idleLooping == false) {
+            this.play({ key: 'idle' });
+            console.log('idle');
+            this.idleLooping = true;
+        }
+        if (this.Running && this.onGround && this.runLooping == false) {
+            this.play({ key: 'run' });
+            this.runLooping = true;
+        }
+
+        //changing look direction
+        if (Phaser.Input.Keyboard.JustDown(keyA)) {
+            this.flipX = true;
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyD)) {
+            this.flipX = false;
+        }
 
         // running
         if(keyA.isDown && this.x >= borderUISize + this.width) {
             this.x -= this.moveSpeed;
+            this.Running = true;
+            this.idleLooping = false;
         } else if (keyD.isDown && this.x <= game.config.width - borderUISize - this.width) {
             this.x += this.moveSpeed;
+            this.Running = true;
+            this.idleLooping = false;
+        }
+        else {
+            this.Running = false;
+            this.runLooping = false;
         }
 
         // sliding
         
         // jumping
-        this.onGround = this.body.touching.down;        //checks if player is standing on solid ground
         if (this.onGround) {            //reset the jump counter when on the ground
             this.jumps = 0;
         }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,16 +4,42 @@ class Play extends Phaser.Scene {
     }
     preload() {
         // temporary images for sprite
-        this.load.image('temp', './assets/temp.png');
+        this.load.image('temp', './assets/temp32.png');
         this.load.image('ground', './assets/temp_ground.png');
-        this.load.spritesheet('runner', 'assets/Player-Sprites/idle-run-temp.png');
+        this.load.spritesheet('runner', 'assets/Player-Sprites/idle-run-temp.png', {
+            frameWidth: 32,
+            frameHeight: 32
+        });
     }
 
     create() {
         //  this.runner = this.add.rectangle(game.config.width / 2, game.config.height* 3 / 4, 80, 160, 0xFFFFFF);
 
+        //ANIMATIONS
+        const runnerIdle = this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNames('runner', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 2,
+            repeat: -1
+        });
+
+        const runnerRun = this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNames('runner', {
+                start: 10,
+                end: 17
+            }),
+            frameRate: 12,
+            repeat: -1
+        });
+
         // temporary sprites //
-        this.runner = new Player1(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'temp').setOrigin(0.5, 0);
+        //this.runner1 = this.add.sprite(game.config.width/2, game.config.height - borderUISize - borderPadding, 'runner').setScale(4).setOrigin(0.5, 1);
+
+        this.runner = new Player1(this, game.config.width/3, game.config.height - 2 *borderUISize - borderPadding, 'temp').setScale(4).setOrigin(0.5, 0.5);
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(400, 630, 'ground').setScale(2).refreshBody();
         this.physics.add.collider(this.runner, this.platforms);
