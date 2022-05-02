a92e2ab29769f3b275213dcfddf8a19eeb8597a4
matty217 <mathill@ucsc.edu>
2022-05-01 21:33:29 -0700

    .

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -437,7 +437,7 @@ class Play extends Phaser.Scene {
         }
         // delete platforms that move out of frame
         this.platformGroup.getChildren().forEach(function(platform){
-            if(platform.x + (platform.width*2) < 0) {
+            if(platform.x + (platform.width*2) < -100) {
                 this.platformGroup.killAndHide(platform);
                 this.platformGroup.remove(platform);
             }
